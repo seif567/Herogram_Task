530e6793d4d3248aea78d75a2514fc66d6334cde
Seif Mohamed <63565044+seif567@users.noreply.github.com>
2025-08-12 16:00:12 +0400

    change cors

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5000',
-  credentials: true
+  origin: '*',          // Allow all origins
+  credentials: false    // Can't use credentials (cookies) with origin '*'
 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
